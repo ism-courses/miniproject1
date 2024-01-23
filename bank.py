3febdd1ccdac7f1f1f73b223825694214f0d4c85
ism-courses <152141243+ism-courses@users.noreply.github.com>
2024-01-23 12:09:57 -0700

    Add files via upload

---
+++ bank.py
@@ -0,0 +1,15 @@
+class CheckingAccount:
+    # class body
+    pass
+
+class SavingAccount:
+    pass
+
+class Employee:
+    def __init__(self,name,age)
+        self.name = name
+        self.age = age
+
+# create a employee object
+employee1 = Employee("bo wen",34)
+print(employee1.name)
